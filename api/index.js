6ce03175e5adccb7622e8a7bd6c98696963041f6
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-09 21:23:54 +0100

    error handler set up for endpoints continue

--- api/index.js
+++ api/index.js
@@ -32,7 +32,14 @@ app.use("/api/rooms", roomsRoute)
 app.use("/api/hotels", hotelsRoute)
 
 app.use((err, req, res, next) => {
-    return res.status(500).json("Hello error from handler!")
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack 
+    })
 })
 
 app.listen(process.env.PORT, () => {
