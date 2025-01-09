e694dce5d6f3963eddf9189e4f9a7c6bf70ed62d
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-09 10:41:04 +0100

    set up database, create routes and models

---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import roomsRoute from "./routes/rooms.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+const app = express();
+dotenv.config()
+
+const connect = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/booking');
+        console.log("Connected to mongoDB.")
+    } catch(error) {
+        throw error;
+    }
+};
+
+// ONLY USE THE FUNCTION BELOW TO KNOW THE PROBLEM WHEN CONNECTION TO THE DATABASE FAILS
+// mongoose.connection.on("disconnected", () => {
+//     console.log("mongoDB is disconnected!")
+// })
+
+// MIDDLEWARES
+app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
+app.use("/api/rooms", roomsRoute)
+app.use("/api/hotels", hotelsRoute)
+
+app.listen(process.env.PORT, () => {
+    connect();
+    console.log("Connected to backend.")
+    console.log(`Website is running on PORT ${process.env.PORT}`)
+})
+
