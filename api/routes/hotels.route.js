927e14290b8f054ecfbb029ce7e5aab4bdfb34ba
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-09 21:45:57 +0100

    advanced error handler integrated with endpoints

--- api/routes/hotels.route.js
+++ api/routes/hotels.route.js
@@ -1,5 +1,6 @@
 import express from "express"
 import Hotel from "../models/hotels.js";
+import { createError } from "../utils/error.js";
 const router = express.Router();
 
 // CREATE 
@@ -47,10 +48,7 @@ router.get("/:id", async (req, res) => {
 // GET ALL HOTELS
 router.get("/", async (req, res, next) => {
     const failed = true;
-    const err = new Error();
-    err.status = 404;
-    err.message = "Sorry not found!";
-    if (failed) return next(err)
+    if (failed) return next(createError(401, "You are not authenticated!"))
 
     try{
         const hotels = await Hotel.find();
