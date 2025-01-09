4722cb610acdfd437209d4cec9ecfcb45c8672b0
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-09 13:36:25 +0100

    started creating routes for hotels endpoints continue

--- api/routes/hotels.route.js
+++ api/routes/hotels.route.js
@@ -1,11 +1,12 @@
 import express from "express"
-import Hotel from "../models/hotels";
+import Hotel from "../models/hotels.js";
 const router = express.Router();
 
 // CREATE 
 router.post(":id?limit=5", async (req, res) => {
-    const hotel = new Hotel(req.body); 
+    const newHotel = new Hotel(req.body); 
     try{
+        const savedHotel = await newHotel.save();
 
     }catch(err){
         res.status(500).json(err);
