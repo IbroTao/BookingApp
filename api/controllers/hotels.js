8d65c6e1f2c349fe2a81f4f9eb6976aaac07468c
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-10 09:49:21 +0100

    endpoints for hotel booking done

--- api/controllers/hotels.js
+++ api/controllers/hotels.js
@@ -1,3 +1,5 @@
+import Hotel from "../models/hotels.js";
+
 export const createHotel = async(req, res, next) => {
     const newHotel = new Hotel(req.body); 
     try{
@@ -24,5 +26,26 @@ export const deleteHotel = async(req, res, next) => {
         res.status(200).json("Hotel has been deleted");
     }catch(err) {
         next(err)
+    }
+}
+
+export const getHotel = async(req, res, next) => {
+    try{
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+    }catch(err){
+        next(err)
+    }
+}
+
+export const getHotels = async(req, res, next) => {
+    const failed = true;
+    if (failed) return next(createError(401, "You are not authenticated!"))
+
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        next(err);
     }
 }
--- api/routes/hotels.route.js
+++ api/routes/hotels.route.js
@@ -1,28 +1,12 @@
 import express from "express"
 import { createError } from "../utils/error.js";
-import { createHotel, deleteHotel, updateHotel } from "../controllers/hotels.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotels.js";
 const router = express.Router();
 
 router.post("/", createHotel);
 router.put("/:id", updateHotel);
 router.delete("/:id", deleteHotel)
-
-// GET A HOTEL
-router.get("/:id", async (req, res) => {
-    
-})
-
-// GET ALL HOTELS
-router.get("/", async (req, res, next) => {
-    const failed = true;
-    if (failed) return next(createError(401, "You are not authenticated!"))
-
-    try{
-        const hotels = await Hotel.find();
-        res.status(200).json(hotels);
-    }catch(err){
-        next(err);
-    }
-})
+router.get("/:id", getHotel)
+router.get("/", getHotels)
 
 export default router;
