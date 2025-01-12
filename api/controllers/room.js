f9fd8ef10e9ed7889f0424d3b33cc1b81c0f49f6
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-12 09:07:27 +0100

    room CRUD endpoints set up

---
+++ api/controllers/room.js
@@ -0,0 +1,19 @@
+import Room from "../models/rooms.js";
+import Hotel from "../models/hotels.js";
+import {createError} from "../utils/error.js";
+
+
+export const createRoom = async(req, res, next) => {
+        const hotelId = req.params.hotelid;
+        const newRoom = new Room(req.body)
+    try {
+        const savedRoom = await newRoom.save();
+        try{
+            await Hotel.findByIdAndUpdate(hotelId, {$push: { rooms: savedRoom._id}})
+        } catch(err) {
+            next(err)
+        }
+    } catch (err) {
+        next(err)
+    }
+}
