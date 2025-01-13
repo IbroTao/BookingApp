f2bd9fbd65cff4525414539492b15338d6f771c1
Taofeek <ibrahimtaofeek005@gmail.com>
2025-01-13 09:11:29 +0100

    room CRUD endpoints continue

--- api/routes/rooms.route.js
+++ api/routes/rooms.route.js
@@ -1,5 +1,12 @@
-import express from "express"
+import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
 const router = express.Router();
 
-export default router
+router.post("/:hotelid", verifyAdmin, createRoom);
+router.put("/:id", verifyAdmin, updateRoom);
+router.delete("/:id", verifyAdmin, deleteRoom)
+router.get("/:id", getRoom)
+router.get("/",  getRooms)
 
+export default router;
